92002e59757052dd4eb3549e330d05aee832c138
Chris Basham <chrisdbasham0317@gmail.com>
2019-12-03 18:09:08 -0700

    Add root path get request/cors setup

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
+app.use(cors())
+app.use(express.json());
+
+app.locals.title = 'Palette Picker';
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Palette Picker!');
+});
