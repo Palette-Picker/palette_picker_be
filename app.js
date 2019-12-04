6b4dd4cb4b3b378b9be04dac2a6570ade0ca4702
Chris Basham <chrisdbasham0317@gmail.com>
2019-12-03 18:06:40 -0700

    Add imports/setup for app file

---
+++ app.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
